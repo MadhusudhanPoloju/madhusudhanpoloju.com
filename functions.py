42d3d7404557e694029d6a44efd4503ee7f48c02
madhusudhan poloju <madhusudhanpoloju210597@gmail.com>
2024-03-12 19:38:53 -0400

    name changes

--- functions.py
+++ functions.py
@@ -2,4 +2,4 @@ def display_invoice(name,amount, due_date):
     print(f" hello {name}")
     print(f" your bill of {amount: .2f} is due: {due_date}")
 
-display_invoice("madhu",123.456, "03/06")
+display_invoice("madhusudhan poloju",123.456, "03/06")
